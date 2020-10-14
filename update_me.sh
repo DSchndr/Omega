f1172617cadd88028861016783410e196c0c5ae1
DSchndr <dennix1010@gmail.com>
2020-10-14 08:58:41 +0200

    Add update_me.sh for rpi branch

---
+++ update_me.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#git clone -b rpi https://github.com/DSchndr/Omega.git
+#git fetch upstream
+#cd Omega
+#git remote add upstream https://github.com/Omega-Numworks/Omega.git
+#git fetch upstream
+#git checkout rpi
+#git merge upstream/omega-master
+#git add *
+#git commit -m "Omega update"
+#git submodule init
+#git submodule update
